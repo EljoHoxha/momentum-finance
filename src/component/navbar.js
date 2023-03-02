fd42c93c677322869946689bcd2b13871a29b7e7
EljoHoxha <eljohoxha15@gmail.com>
2023-03-02 11:35:09 +0100

    navbar-mobile

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import MomentumFinance from "../logo/MomentumFinance-logo.svg";
 const Navbar = ({ learnAboutUs, comehome, onContactUs }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const hamburgerClick = () => {
+    setIsOpen(!isOpen);
+  };
+  const homeClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const aboutUsClick = () => {
+    return isOpen === true && setIsOpen(!isOpen), learnAboutUs();
+  };
+  const contactUsClick = () => {
+    return isOpen === true && setIsOpen(!isOpen), onContactUs();
+  };
+
   return (
-    <div className="navbar" ref={comehome}>
-      <div className="full-container">
-        <div className="navbar-section">
-          <div className="navbar-section-item">
+    <div className={`navbar ${isOpen && "open"}`} ref={comehome}>
+      <div className="navbar-container">
+        <div className="navbar-container-section">
+          <div className="navbar-container-section-item">
             <img
               alt=""
-              className="navbar-section-item-img"
+              className="navbar-container-section-item-img"
               src={MomentumFinance}
+              onClick={homeClick}
             />
+            <div
+              onClick={hamburgerClick}
+              className={`navbar-container-section-item-hamburger ${
+                isOpen && "open"
+              }`}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
-          <div className="navbar-section-item  navbar-section-item-group">
-            <p className="navbar-section-item-text">Blog</p>
-            <p onClick={learnAboutUs} className="navbar-section-item-text">
+          <div
+            className={`navbar-container-section-item  navbar-container-section-item-group`}
+          >
+            <p className="navbar-container-section-item-text">Blog</p>
+            <p
+              onClick={aboutUsClick}
+              className="navbar-container-section-item-text"
+            >
               About Us
             </p>
-            <button onClick={onContactUs} className="navbar-section-item-btn">
+            <button
+              onClick={contactUsClick}
+              className="navbar-container-section-item-btn"
+            >
               Schedule a Call
             </button>
           </div>
